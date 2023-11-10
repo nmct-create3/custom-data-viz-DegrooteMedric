849d4c561d3db8df6f89808c228d5840b9bfa56d
Simon <coudeville.simon@gmail.com>
2023-11-10 11:33:49 +0100

    Added more comments

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 // use these elements to render the chart, legend and controls
-
 const $legendEl = document.querySelector(`.js-legend`);
 const $chartEl = document.querySelector(`.js-chart`);
 const $controlsEl = document.querySelector(`.js-controls`);
 const $toggleEl = document.querySelector(`.js-theme-toggle`);
+// empty object to store the data
 let data;
 
 // SVG drawing functions
@@ -21,8 +21,8 @@ const drawArc = (([cx, cy], [rx, ry], [t1, Δ], φ,) => {
     returns a SVG path element that represent a ellipse.
     cx,cy → center of ellipse
     rx,ry → major minor radius
-    t1 → start angle, in radian.
-    Δ → angle to sweep, in radian. positive.
+    t1 → start angle, in degrees.
+    Δ → angle to sweep, in degrees. positive.
     φ → rotation on the whole, in radian
     Based on:
     URL: SVG Circle Arc http://xahlee.info/js/svg_circle_arc.html
@@ -33,6 +33,8 @@ const drawArc = (([cx, cy], [rx, ry], [t1, Δ], φ,) => {
     // convert Δ from degree to radian
     Δ = Δ / 360 * 2 * π;
     Δ = Δ % (2 * π);
+    // convert φ from degree to radian
+    φ = φ / 360 * 2 * π;
     const rotMatrix = f_rotate_matrix(φ);
     const [sX, sY] = (f_vec_add(f_matrix_times(rotMatrix, [rx * cos(t1), ry * sin(t1)]), [cx, cy]));
     const [eX, eY] = (f_vec_add(f_matrix_times(rotMatrix, [rx * cos(t1 + Δ), ry * sin(t1 + Δ)]), [cx, cy]));
@@ -42,18 +44,22 @@ const drawArc = (([cx, cy], [rx, ry], [t1, Δ], φ,) => {
     return d;
 });
 
-// render the chart
+// get the data with async and start the chain of functions
+// pass the data to the next function in the chain
+
+
+// render the controls (radio buttons) and make them listen to changes to update the chart and legend
+
+// render the legend for the first party and then update it with new data
+// make a list and add a list item per key and pass a color in the form of a css variable
+
+// render the chart for the first party and then update it with new data
 // make a new svg of 400x200
 // for each key in the keys object, draw an arc with a stroke width of 50
 // use the drawArc([cx, cy], [rx, ry], [start angle, angle to sweep], rotation in the whole)
 
 // update the arcs with new data
-
-// render the legend
-
-// render the controls (radio buttons) and make them listen to changes to update the chart and legend
-
-// get the data with async
+// change the stroke-dasharray to the new values
 
 // theme toggle
 // the theme toggle should remember your choice in localStorage
